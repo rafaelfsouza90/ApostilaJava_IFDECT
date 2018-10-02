4e09a2f2d74989601229bfb11fcac162506e81b7
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-10-02 15:02:37 -0300

    Diferença entre os tipos de Escrita de Dados.

---
+++ EscritaDeDados.java
@@ -0,0 +1,14 @@
+package escritadedados;
+public class EscritaDeDados{
+    public static void main(String[]args){
+    int numero=10;
+    System.out.println("O número é: ");
+    System.out.println(numero);
+    
+    System.out.println("O número é: "+numero);
+    
+    System.out.println("O número + 1 é: " + (numero+1));
+    
+    
+    }
+}
